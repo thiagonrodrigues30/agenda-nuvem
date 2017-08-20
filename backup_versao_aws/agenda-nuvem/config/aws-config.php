45c417ecdd352de56872193a5c2269f206defd52
Thiago Nóbrega <thiago_nrodrigues@yahoo.com.br>
2017-08-20 20:56:01 -0300

    Algumas alteraçoes

--- backup_versao_aws/agenda-nuvem/config/aws-config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define("S3_ACCESS_KEY", "AKIAJ5TN5MD6AGRPFA6Q");
-define("S3_SECRET_KEY", "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");
-
-?>
--- backup_versao_aws/agenda-nuvem/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-    include("views/index.php");
-
-?>
